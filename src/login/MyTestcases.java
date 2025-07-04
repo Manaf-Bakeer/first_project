1f5fd30fc394f13a432344a670f5cca60accb026
Manaf-Bakeer <manafgithub@gmail.com>
2025-07-04 22:50:53 +0300

    update

---
+++ src/login/MyTestcases.java
@@ -0,0 +1,31 @@
+package login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyTestcases {
+	
+	WebDriver driver = new ChromeDriver();
+	
+	String TheURL = "https://automationteststore.com/";
+	
+	@BeforeTest
+	public void mySetup() {
+		
+		driver.get(TheURL);
+		
+		driver.manage().window().maximize();
+	}
+	
+	
+	@Test
+	public void myFirstTest() {}
+	
+	
+	
+	
+	
+
+}
